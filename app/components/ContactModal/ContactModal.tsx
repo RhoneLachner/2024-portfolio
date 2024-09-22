d7d3422b5cc9a84286182101349c61528a29695e
rhone <fernandclay@gmail.com>
2024-09-22 13:14:58 -0700

    refactor: remove unused CSS module file and unnecessary divs from contactModal

--- app/components/ContactModal/ContactModal.tsx
+++ app/components/ContactModal/ContactModal.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ContactForm from '../ContactForm/ContactForm';
-import styles from './ContactModal.module.css';
 import DynamicCloseButton from '../DynamicIcons/DynamicCloseButton';
 import DynamicSocialIcons from '../DynamicIcons/DynamicSocialIcons'; // Import the dynamic social icons
 
@@ -16,14 +15,7 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
     <div className="modalOverlay">
       <div className="modalContent">
         <DynamicCloseButton onClose={onClose} />
-
-        <div className={styles.socialIconsContainer}>
-          <DynamicSocialIcons />{' '}
-        </div>
-
-        <div className={styles.contactFormContainer}>
-          <ContactForm />
-        </div>
+        <DynamicSocialIcons /> <ContactForm />
       </div>
     </div>
   );
