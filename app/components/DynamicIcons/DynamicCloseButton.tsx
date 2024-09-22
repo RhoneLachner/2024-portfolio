d295916006f999c47c49ba80309df80e8003bb2c
rhone <fernandclay@gmail.com>
2024-09-22 15:31:33 -0700

    docs: Add descriptive comments to DynamicCloseIcon component

--- app/components/DynamicIcons/DynamicCloseButton.tsx
+++ app/components/DynamicIcons/DynamicCloseButton.tsx
@@ -1,31 +1,47 @@
+/**
+ * DynamicCloseButton Component
+ *
+ * The close button triggers the `onClose` function when clicked, and it is used in all modal components.
+ * This component renders a button with either a white or black close icon, depending on the current theme (light or dark).
+ * The theme is determined by observing changes to the `data-theme` attribute.
+ */
+
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import { ICONS } from '../../assets/icons'; // Importing icon paths
+import { ICONS } from '../../assets/icons';
 
 interface CloseButtonProps {
-  onClose: () => void;
+  // Function to handle the close action
+  onClose: () => void; 
 }
 
 const DynamicCloseButton: React.FC<CloseButtonProps> = ({ onClose }) => {
+  // State to store the current theme (light or dark)
   const [theme, setTheme] = useState<string | null>(null);
 
   useEffect(() => {
     const getCurrentTheme = () =>
       document.documentElement.getAttribute('data-theme');
+    
+    // Set initial theme when the component mounts
     setTheme(getCurrentTheme());
 
+    // MutationObserver to detect changes to the `data-theme` attribute
     const observer = new MutationObserver(() => {
       setTheme(getCurrentTheme());
     });
 
+    // Observe the document for theme changes
     observer.observe(document.documentElement, {
-      attributes: true,
-      attributeFilter: ['data-theme'],
+      attributes: true, 
+      attributeFilter: ['data-theme'], 
     });
 
+    // Cleanup the observer when the component unmounts
     return () => observer.disconnect();
   }, []);
 
+  // Icon is chosen by current theme
   const closeIcon = theme === 'dark' ? ICONS.closeWhite : ICONS.closeBlack;
 
   return (
