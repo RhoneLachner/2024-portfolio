98be3ed0c9b34118cc2a3c8d7037acdbb147c2bd
rhone <fernandclay@gmail.com>
2024-09-23 15:19:33 -0700

    p erf: replace tff fonts with woff, and update cache-control

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-       reactStrictMode: false, 
-     };
-     
-     export default nextConfig; 
-     
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/fonts/:font*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=3600, must-revalidate',
+          },
+          {
+            key: 'Link',
+            value:
+              '</fonts/Montserrat-Regular.woff2>; rel=preload; as=font; type="font/woff2"; crossorigin="anonymous"',
+          },
+          {
+            key: 'Link',
+            value:
+              '</fonts/Hyperbola.woff2>; rel=preload; as=font; type="font/woff2"; crossorigin="anonymous"',
+          },
+        ],
+      },
+    ];
+  },
+};
+
+export default nextConfig;
