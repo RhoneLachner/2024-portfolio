74c05a1010c7b38bd930d9bba9f097a0215b7854
rhone <fernandclay@gmail.com>
2025-04-21 17:27:44 -0700

    [perf]: Add cloud.umami analytics for traffic counting

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css';
 import styles from './layout.module.css';
 import Header from './components/Header/Header';
 import { ModalProvider } from './context/ModalContext';
+import Script from 'next/script';
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
@@ -13,6 +14,12 @@ export default function Layout({ children }: { children: ReactNode }) {
           <Header />
           <main className={styles.main}>{children}</main>
         </ModalProvider>
+        <Script
+          src="https://cloud.umami.is/script.js"
+          data-website-id="c55fe9c8-adb4-4b1f-bb69-374091dda47f"
+          strategy="afterInteractive"
+          defer
+        />
       </body>
     </html>
   );
